31de25bdffc77fcea5184cde6931574eb8314178
Nelson Liu <nfliu@nelsonliu.me>
2024-05-01 02:09:49 -0700

    Use zip instead of tar, cache the testing venv

--- test_and_make_submission.sh
+++ test_and_make_submission.sh
@@ -2,39 +2,36 @@
 set -euo pipefail
 
 echo "Creating virtual environment to run tests"
-rm -rf 336_a2_test_venv
 python3 -m venv ./336_a2_test_venv
 source ./336_a2_test_venv/bin/activate
 echo "Installing requirements"
 pip install --upgrade pip
 pip install -e ./cs336-basics/ -e ./cs336-systems/'[test]'
 echo "Running tests"
-pytest -v ./cs336-systems/tests --junitxml=test_results.xml || 0
+pytest -v ./cs336-systems/tests --junitxml=test_results.xml || true
 echo "Done running tests"
 echo "Cleaning up virtual environment for tests"
 deactivate
-rm -rf 336_a2_test_venv
 
 # Set the name of the output tar.gz file
-output_file="cs336-spring2024-assignment-2-submission.tar.gz"
-rm "$output_file"
+output_file="cs336-spring2024-assignment-2-submission.zip"
+rm "$output_file" || true
 
-# Compress all files in the current directory into a single tar.gz file
-tar \
-    --exclude='*egg-info*' \
-    --exclude='*mypy_cache*' \
-    --exclude='*pytest_cache*' \
-    --exclude='*build*' \
-    --exclude='*ipynb_checkpoints*' \
-    --exclude='*__pycache__*' \
-    --exclude='*__pycache__*' \
-    --exclude='*.pkl' \
-    --exclude='*.pickle' \
-    --exclude='*.txt' \
-    --exclude='*.log' \
-    --exclude='*.json' \
-    --exclude='*.out' \
-    --exclude='*.err' \
-    -czvf "$output_file" *
+# Compress all files in the current directory into a single zip file
+zip -r "$output_file" . \
+    -x '*egg-info*' \
+    -x '*mypy_cache*' \
+    -x '*336_a2_test_venv*' \
+    -x '*pytest_cache*' \
+    -x '*build*' \
+    -x '*ipynb_checkpoints*' \
+    -x '*__pycache__*' \
+    -x '*.pkl' \
+    -x '*.pickle' \
+    -x '*.txt' \
+    -x '*.log' \
+    -x '*.json' \
+    -x '*.out' \
+    -x '*.err'
 
 echo "All files have been compressed into $output_file"
